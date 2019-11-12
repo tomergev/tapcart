afb6ad3f00a36d848e1be099d4c2741a297803c3
tomergev <tomergev21@gmail.com>
2019-11-12 13:41:53 -0800

    Updates

---
+++ precache-manifest.36293f0a7a74cfa88210df56cd35c774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60c0bf7a885a8b95972bc3c485ef1f4",
+    "url": "/tapcart/index.html"
+  },
+  {
+    "revision": "ce483a14eca4a3617360",
+    "url": "/tapcart/static/css/main.34fcb005.chunk.css"
+  },
+  {
+    "revision": "e68f49132c53dde15806",
+    "url": "/tapcart/static/js/2.ff30e051.chunk.js"
+  },
+  {
+    "revision": "ce483a14eca4a3617360",
+    "url": "/tapcart/static/js/main.f0ba751b.chunk.js"
+  },
+  {
+    "revision": "8a6ef4f260f255d36a0a",
+    "url": "/tapcart/static/js/runtime-main.49ec0c2b.js"
+  }
+]);
